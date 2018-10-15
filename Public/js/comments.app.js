177c4bb819bff20096aa843f645575a9924f357b
Jennie Chen <chen417@iu.edu>
2018-10-15 11:54:55 -0400

    edit commentsapp.js

--- Public/js/comments.app.js
+++ Public/js/comments.app.js
@@ -1,50 +1,23 @@
 var tasksApp = new Vue({
-  el: '#taskMain',
+  el: '#commentMain',
   data: {
     task: {
       id: 0,
-      title: 'foo',
-      type : '',
-      size : '',
-      team : '',
-      status: '',
-      start_date: '',
-      close_date: null,
-      hours_worked: '',
-      perc_complete: '',
-      current_sprint : ''
+      comment: 'foo',
     },
-    work: [ ],
-    workForm: { },   // populated by this.getEmptyWorkForm()
-    teamList: [] // All the teams
+    comment: [ ],
+    commentForm: { },   // populated by this.getEmptyWorkForm()
   },
-  computed: {
-    workSpan () {
-      return moment(this.workForm.stop + ' ' + this.workForm.stop_time)
-             .diff(moment(this.workForm.start + ' ' + this.workForm.start_time), 'hours', true)
-             .toFixed(1);
-    }
-  },
+
   methods: {
-    handleWorkForm(e) {
+    handleCommentForm(e) {
 
-      // TODO: Check validity in a better way
-      if (this.workSpan <= 0) {
-        console.error('Cannot submit, invalid values');
-        return;
-      }
-
-      this.workForm.start_date = this.workForm.start + ' ' + this.workForm.start_time;
-      this.workForm.hours = this.workSpan;
-      // Stop field not used by the API
-      // this.workForm.stop_date = this.workForm.stop + ' ' + this.workForm.stop_time;
-
-      const s = JSON.stringify(this.workForm);
+      const s = JSON.stringify(this.commentForm);
 
       console.log(s);
 
       // POST to remote server
-      fetch('api/work.php', {
+      fetch('api/comment.php', {
         method: "POST", // *GET, POST, PUT, DELETE, etc.
         headers: {
             "Content-Type": "application/json; charset=utf-8"
@@ -52,69 +25,48 @@ var tasksApp = new Vue({
         body: s // body data type must match "Content-Type" header
       })
       .then( response => response.json() )
-      .then( json => {this.work.push(json)})
+      .then( json => {this.comment.push(json)})
       .catch( err => {
-        console.error('WORK POST ERROR:');
+        console.error('COMMENT POST ERROR:');
         console.error(err);
       })
 
       // Reset workForm
-      this.workForm = this.getEmptyWorkForm();
+      this.commentForm = this.getEmptyCommentForm();
     },
-    sumHours() {
-      return this.work.reduce( (sum, current) => sum + current.hours, 0 )
-    },
-    diffAsHours() {
-      return 0 //moment().duration(end.diff(startTime)).asHours();
-    },
+
     getEmptyWorkForm() {
       return {
-        start: moment().format('YYYY-MM-DD'),
-        start_time: moment().format('HH:mm'),
-        stop: moment().format('YYYY-MM-DD'),
-        stop_time: moment().format('HH:mm'),
-        team_id: null,
-        task_id: this.task.id,
-        completion_estimate: 0
+        id: NULL,
+        comment: NULL
       }
     },
-    prettyDate(d) {
-      return moment(d).format('YYYY-MM-DD HH:mm')
-    }
-  },
+
   created () {
 
     // Do data fetch
     const url = new URL(window.location.href);
-    const taskId = url.searchParams.get('taskId');
-    console.log('Task: '+ taskId);
-    this.task.id = taskId;
+    const id = url.searchParams.get('id');
+    console.log('Comment: '+ id);
+    this.comment.id = id;
 
-    if (!taskId) {
+    if (!id) {
       //TODO: Error? 404?
       //e.g., window.location = '404.html';
     }
 
     // Populate workForm with default values
-    this.workForm = this.getEmptyWorkForm();
+    this.commentForm = this.getEmptyCommentForm();
 
     // TODO: Fetch task-specific data
     // fetch('api/task?id=4')
-    fetch('api/work.php?taskId='+taskId)
+    fetch('api/comment.php?id='+id)
     .then( response => response.json() )
-    .then( json => {tasksApp.work = json} )
+    .then( json => {commentsApp.comment = json} )
     .catch( err => {
-      console.error('WORK FETCH ERROR:');
+      console.error('COMMENT FETCH ERROR:');
       console.error(err);
     })
-
-    // Fetch all teams, for the form
-    fetch('api/team.php')
-    .then( response => response.json() )
-    .then( json => {tasksApp.teamList = json} )
-    .catch( err => {
-      console.log('TEAM LIST ERROR:');
-      console.log(err);
-    })
   }
+}
 })
